9dd29d073102f0c236a23c7b6b02a4e57bcff6c8
24-002580\user <annarainchik@gmail.com>
2022-04-04 20:34:30 +0300

    change progect

--- src/test/java/tests/PracticeForm.java
+++ src/test/java/tests/PracticeForm.java
@@ -13,60 +13,50 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeForm {
 
+
     @BeforeAll
 
-    static  void beforeAll() {
+    static void beforeAll() {
 
         Configuration.baseUrl = "https://demoqa.com";
-
-
     }
+
     @Test
+    void successFillTest() {
 
-
-    void successFillTest (){
-
-        open ("/automation-practice-form");
+        open("/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
-
-
         $("#firstName").setValue("Anna");
         $("#lastName").setValue("Rain");
         $("#userEmail").setValue("annatest111@gmail.com");
-
         $("#genterWrapper").$(byText("Female")).click();
-
-        $("#userNumber").setValue("78889998877");
-
+        $("#userNumber").setValue("7888999887");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOption("1984");
-
         $("[aria-label$='January 14th, 1984']").click();
-
-
-
         $("#subjectsInput").setValue("Arts").pressEnter();
         $("#hobbiesWrapper").$(byText("Music")).click();
-
-        $("#uploadPicture").uploadFile(new File("src/test/java/tests/khosiko5.jpg"));
-
-        $("#currentAddress").setValue("Address");
-
-
+        $("#uploadPicture").uploadFromClasspath("khosiko5.jpg");
+        $("#currentAddress").setValue("Address2");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
-
         $("#submit").pressEnter();
-
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-
-
         $("[class='modal-body']").shouldHave(
                 text("Anna Rain"),
-                text("annatest111@gmail.com"));
+                text("annatest111@gmail.com"),
+                text("Female"),
+                text("7888999887"),
+                text("14 January,1984"),
+                text("Arts"),
+                text("Music"),
+                text("khosiko5.jpg"),
+                text("Address2"),
+                text("NCR Delhi"));
+
 
 
     }
