e0b2396b82eab1728119ca4b62c10965621cd92c
24-002580\user <annarainchik@gmail.com>
2022-04-04 22:10:54 +0300

    change progect 2

--- src/test/java/tests/PracticeForm.java
+++ src/test/java/tests/PracticeForm.java
@@ -3,24 +3,16 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeForm {
-
-
     @BeforeAll
-
     static void beforeAll() {
-
         Configuration.baseUrl = "https://demoqa.com";
     }
-
     @Test
     void successFillTest() {
 
@@ -56,9 +48,5 @@ public class PracticeForm {
                 text("khosiko5.jpg"),
                 text("Address2"),
                 text("NCR Delhi"));
-
-
-
     }
-
 }
